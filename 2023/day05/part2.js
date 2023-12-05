b60967ab6424b2614ea033bb8c949580950ce8ba
Metal Daze <stmorawi@gmail.com>
2023-12-05 21:10:29 +0100

    day 5 part 2
    inperformant, aber immerhin auf Anhieb richtig... :D

--- 2023/day05/part2.js
+++ 2023/day05/part2.js
@@ -5,8 +5,11 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
     console.error(err);
     return;
   }
+
+  let start = Date.now();
+
   let sections = data.split("\n\n");
-  let seeds = sections[0].substring(sections[0].indexOf(":")+1).split(" ").map(n => n.trim()).filter(n => n != '').map(n => Number(n));
+  let seeds = mapSeeds(sections[0]).sort((a,b) => a.start-b.start);
   let seed2soil = asMap(sections[1]);
   let soil2fertilizer = asMap(sections[2]);
   let fertilizer2water = asMap(sections[3]);
@@ -15,9 +18,35 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
   let temperature2humidity = asMap(sections[6]);
   let humidity2location = asMap(sections[7]);
 
-  let seedLocations = seeds.map(s => map(map(map(map(map(map(map(s, seed2soil),soil2fertilizer),fertilizer2water),water2light),light2temperature),temperature2humidity),humidity2location)).sort((a,b) => (a-b));
-  console.log("result: " + seedLocations[0]);
+  let minSeedLocation = -1;
+  for (let i=0; i<seeds.length-1; i+=2) {
+    for (let j=seeds[i].start; j<seeds[i].end; j++) {
+      let loc = map(map(map(map(map(map(map(j, seed2soil),soil2fertilizer),fertilizer2water),water2light),light2temperature),temperature2humidity),humidity2location);
+      if (minSeedLocation == -1 || loc<minSeedLocation) {
+        minSeedLocation = loc;
+      }
+    }
+
+  }
+  let end = Date.now();
+  console.log("result: " + minSeedLocation);
+  console.log("Runtime: " + (end-start) + "ms");
+  // result: 78775051
+  // Runtime: 645245ms
 });
+
+function mapSeeds(line) {
+  let result = [];
+  let numbers = line.substring(line.indexOf(":")+1).split(" ").map(n => n.trim()).filter(n => n != '').map(n => Number(n));
+  for (let i=0; i<numbers.length-1; i+=2) {
+    result.push({
+      start: numbers[i],
+      end: numbers[i] + numbers[i+1]
+    });
+  }
+
+  return result;
+}
 
 function asMap(section) {
   let lines = section.split("\n").splice(1);
